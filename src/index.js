344854229f0cf02324d96342e8afb32193b0824f
ssj9685 <ssj9685@naver.com>
2020-08-24 18:43:40 +0900

    component and router update

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import './index.css';
 import { Provider } from 'react-redux';
 import {createStore} from 'redux'
 import reducer from './reducer/reducer';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Login from './component/common/Login';
 
 const store = createStore(reducer);
 
 const render = ()=>{
     ReactDOM.render(
         <Provider store={store}>
-            <Insurance/>
+            <BrowserRouter>
+                <Route exact path="/" component={Insurance}/>
+                <Route path="/panel" component={Login}/>
+                <Route path="/dashboard" component={Login}/>
+            </BrowserRouter>
         </Provider>,
         document.getElementById('root')
     );        
